74fd8d875ae2649a94f9cc34e0a60f1928eff8a2
Sakala85 <42222200+Sakala85@users.noreply.github.com>
2025-01-20 15:02:13 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,23 +2,23 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
+    const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || "GTM-W558ZCML";
+
     return (
       <Html>
         <Head>
+          {/* Add support for Privacy Sandbox and first-party data */}
+          <meta name="privacy-sandbox" content="enabled" />
           <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-W558ZCML');`,
-            }}
+            async
+            src={`https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`}
+            data-cookieless="true"
           />
         </Head>
         <body>
           <noscript>
             <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-W558ZCML"
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
               height="0"
               width="0"
               style={{ display: "none", visibility: "hidden" }}
