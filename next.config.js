10f910040ea08f560cd30da666b7a44db9b7fbb9
Sakala85 <42222200+Sakala85@users.noreply.github.com>
2025-01-20 15:37:12 +0100

    fix scrips

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "script-src 'self' 'wasm-unsafe-eval' 'inline-speculation-rules' chrome-extension: https://www.googletagmanager.com https://*.consentframework.com;",
-          },
-        ],
-      },
-    ];
-  },
-};
+// module.exports = {
+//   async headers() {
+//     return [
+//       {
+//         source: "/:path*",
+//         headers: [
+//           {
+//             key: "Content-Security-Policy",
+//             value:
+//               "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' 'wasm-unsafe-eval' 'inline-speculation-rules' https://*.googletagmanager.com https://*.consentframework.com chrome-extension:; script-src-elem 'self' 'unsafe-inline' https://*.googletagmanager.com https://*.consentframework.com chrome-extension:;",
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
